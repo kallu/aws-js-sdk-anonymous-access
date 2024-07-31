7c3279eb8379a1d995b9fd2a79b997662e4342ef
kallu <petri.kallberg@gmail.com>
2024-07-31 18:44:54 +0300

    Rewritten and reformated

--- src/s3.ts
+++ src/s3.ts
@@ -1,31 +1,34 @@
-import { S3 } from "@aws-sdk/client-s3";
+import {
+    S3
+} from "@aws-sdk/client-s3";
 
-export function fetchFromBucket(bucket: string, key: string) {
-  const region = "eu-north-1";
+export function ListBucket(list: string, bucket: string, prefix: string, region: string) {
 
-  // Initialize an S3 service with credentials for our identity pool.
-  const s3 = new S3({
-    region: region,
-    signer: { sign: async (request) => request }
-    //credentials: fromCognitoIdentityPool({
-    //  client: new CognitoIdentityClient({ region: region }),
-    //  identityPoolId: identityPoolId,
-    //}),
-  });
+    // Initialize an S3 service with credentials for our identity pool.
+    const s3 = new S3({
+        region: region,
+        signer: {
+            sign: async (request) => request
+        }
+        //credentials: fromCognitoIdentityPool({
+        //  client: new CognitoIdentityClient({ region: region }),
+        //  identityPoolId: identityPoolId,
+        //}),
+    });
 
-  // Fetch and print out the object size
-  s3.getObject(
-    {
-      Bucket: bucket,
-      Key: key,
-    },
-    (err, data) => {
-      if (err) {
-        console.log(`Error when fetching from bucket: ${err.stack}`);
-      } else {
-        console.log(`Data fetched from bucket. Size: [${data.ContentLength}]`);
-      }
+    // List objects in the bucketa
+    const params = {
+        Bucket: bucket,
+        Delimiter: '/',
+        Prefix: prefix
     }
-  );
-}
 
+    var listing = document.getElementById(list);
+    s3.listObjectsV2(params, function(err, data) {
+        if (err) console.log(err, err.stack);
+        console.log(data);
+        data.Contents.forEach((obj) => {
+            listing.innerHTML += "<tr><td>" + obj.Key + "</td><td>" + obj.Size + "</td><td>" + obj.StorageClass + "</td></tr>";
+        });
+    });
+};
